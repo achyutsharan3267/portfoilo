3610ed0ee44f13feb22dc5639fd92b8cc8b23bfa
achyutsharan3267 <achyut.sharan@impressico.com>
2023-08-05 13:40:21 +0530

    new file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeContentComponent } from './home-content/home-content.component';
+const routes: Routes = [
+  {path:'header',component:HeaderComponent},
+  {path:'footer',component:FooterComponent},
+  {path:'home',component:HomeContentComponent},
+  // {path:'', component:HomeContentComponent}
+  {path:'', redirectTo:'home',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeContentComponent
   ],
   imports: [
     BrowserModule,
